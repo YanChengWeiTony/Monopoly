19c7f6045fa2d5762fdca2ad1dfa2da4703cf93c
YanChengWeiTony <tonywei1997@gmail.com>
2017-07-26 22:27:16 +0800

    first upload

---
+++ Monopoly_0/Assets/Scripts/MapGameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//using DG.Tweening;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using System;
+public class MapGameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeScene(string myscene){
+		if (myscene == "main") {
+			SceneManager.LoadScene ("main");
+		}
+		if (myscene == "map") {
+			SceneManager.LoadScene ("map");
+		}
+	}
+}
