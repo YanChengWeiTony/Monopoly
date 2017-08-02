445c60a0464c7427e076b7c14732e3ec9720f00d
YanChengWeiTony <tonywei1997@gmail.com>
2017-08-03 02:15:09 +0800

    2017/8/3

--- Monopoly_1/Assets/Scripts/GameController.cs
+++ Monopoly_1/Assets/Scripts/GameController.cs
@@ -10,6 +10,7 @@ public class GameController : MonoBehaviour {
 	private Vector3 v1 = new Vector3 (0.29f, -0.68f, -6.244985f);
 	private Vector3 v2 = new Vector3 (0.29f, 11f, -6.244985f);
 	private Vector3 v3 = new Vector3 (0.29f, -12.36f, -6.244985f);
+	private bool done = false;
 
 	//fundamental setting
 	public int myname;
@@ -23,10 +24,15 @@ public class GameController : MonoBehaviour {
 	public GameObject others_display;
 	public Image buyin_messenge_display;
 	public Image sellout_messenge_display;
+	public Image confirm_messenge_display;
+	public GameObject MyNet;
+	public InputField MyBuyInput;
+	public InputField MySellInput;
 
 	//stocks
 	public double[,] stock_inf;//1: slope, 2: price, 3: remain;
 	public GameObject[] mystock;
+	public int[,] stock_slope;
 
 	//text
 	public Text Name;
@@ -37,6 +43,8 @@ public class GameController : MonoBehaviour {
 	public Text Stock_digit;
 	public Text Knife_digit;
 	public Text timer;
+	public Text buyin_messenger;
+	public Text sellout_messenger;
 
 	//button
 	public Button Back_others;
@@ -44,22 +52,37 @@ public class GameController : MonoBehaviour {
 	public Button buyin_no;
 	public Button sellout_yes;
 	public Button sellout_no;
+	public Button Confirm;
+	public Button All_Update;
+	public Button Stock_Update;
 
-	//int
+	//int and string
 	public int Total_Asset;
 	public int Cash;
 	public int Bank;
 	public int Estate;
 	public int Stock;
 	public int Knife;
+	public int time_idx;
+	public string stock_buying_selling_pk;
+	public int stock_buying_selling_state; //1 for buying, -1 for selling
+
+	//url
+	private const string myurl = "http://davidbrother.pythonanywhere.com/admin/";
 
 	// Use this for initialization
 	void Start () {
 		Name.text = String.Format ("{0:D}", myname);
 		ID = "1";
+
 		stock_inf = new double[4, 7];
-		stock_inf [1, 3] = 3.25;
-		stock_inf [2, 4] = 4315;
+
+		stock_slope = new int[31, 7];
+
+		time_idx = 1;
+
+		Network_Update MyNetScript = MyNet.GetComponent<Network_Update>();
+		MyNetScript.Connection (Name.text, time_idx);
 	}
 	
 	// Update is called once per frame
@@ -68,24 +91,31 @@ public class GameController : MonoBehaviour {
 		DateTime nowDateTime = DateTime.Now;
 		timer.text = String.Format ("{0:D4}/{1:D2}/{2:D2} {3:D2}:{4:D2}:{5:D2}", nowDateTime.Year, nowDateTime.Month, nowDateTime.Day, nowDateTime.Hour, nowDateTime.Minute, nowDateTime.Second);		
 
-		Total_Asset = Cash + Bank + Estate + Stock;
+		//confirm update frequency	
+		if (nowDateTime.Minute % 5 == 4)
+			done = false; 
 
-		//url per 5 minutes
+		//per 5 minutes update -> url, instant in the bank, Knife effect, stock_inf development;
+		if (nowDateTime.Minute % 5 == 0 && !done) {
+			done = true;
+			time_idx++;
 
-		//print on the screen
-		Total_Asset_digit.text = String.Format("{0:D}", Total_Asset);
-		Cash_digit.text = String.Format ("{0:D}", Cash);
-		Bank_digit.text = String.Format ("{0:D}", Bank);
-		Estate_digit.text = String.Format("{0:D}", Estate);
-		Stock_digit.text = String.Format("{0:D}", Stock);
-		Knife_digit.text = String.Format("{0:D}", Knife);
+			Network_Update MyNetScript = MyNet.GetComponent<Network_Update>();
+			MyNetScript.Connection (Name.text, time_idx);
+		}
 
+
+		//calculate and display stock digit
+		Stock = 0;
 		for (int i = 1; i < mystock.Length; i++) {
 			Text[] buffer = mystock [i].GetComponentsInChildren<Text> ();
-			buffer [1].text = String.Format ("{0:F}%", stock_inf[1, i]);
-			buffer [2].text = String.Format ("{0:F}", stock_inf[2, i]);
-			buffer [3].text = String.Format ("{0:F}", stock_inf[3, i]);
+			Stock += (int)Convert.ToUInt32 (buffer [2].text) * (int)Convert.ToUInt32 (buffer [3].text);
 		}
+		Stock_digit.text = String.Format ("{0:D}", Stock);
+
+		//calculate and display Total_Asset digit
+		Total_Asset = (int)Convert.ToUInt32(Cash_digit.text) + (int)Convert.ToUInt32(Bank_digit.text) + (int)Convert.ToUInt32(Estate_digit.text) + Stock;
+		Total_Asset_digit.text = String.Format("{0:D}", Total_Asset);
 	}
 
 	public void ChangePage(string ID){
@@ -116,14 +146,38 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
+	public void buyin__money(string pk){
+		stock_buying_selling_pk = pk;
+		stock_buying_selling_state = 1;
+	}
+
+	public void Sellout__money(string pk){
+		stock_buying_selling_pk = pk;
+		stock_buying_selling_state = -1;
+	}
+
+	public void Send_Stock_money(){
+		if (stock_buying_selling_state == 1) {
+			Network_Update MyNetScript = MyNet.GetComponent<Network_Update>();
+			MyNetScript.Buy_Stock_Connection (Name.text, stock_buying_selling_pk ,MyBuyInput.text);
+		}
+		if (stock_buying_selling_state == -1) {
+			Network_Update MyNetScript = MyNet.GetComponent<Network_Update>();
+			MyNetScript.Buy_Stock_Connection (Name.text, stock_buying_selling_pk ,"-" + MySellInput.text);
+		}
+
+	}
+
 	public void buyin_sellout(int mode){
 		if (mode == 0) {
 			buyin_messenge_display.gameObject.SetActive (false);
 			sellout_messenge_display.gameObject.SetActive (false);
+			confirm_messenge_display.gameObject.SetActive (false);
 		}
 		if (mode == 1) {
 			buyin_messenge_display.gameObject.SetActive (true);
 			sellout_messenge_display.gameObject.SetActive (false);
+			confirm_messenge_display.gameObject.SetActive (false);
 
 			buyin_yes.interactable = true;
 			buyin_no.interactable = true;
@@ -131,9 +185,38 @@ public class GameController : MonoBehaviour {
 		if (mode == 2) {
 			buyin_messenge_display.gameObject.SetActive (false);
 			sellout_messenge_display.gameObject.SetActive (true);
+			confirm_messenge_display.gameObject.SetActive (false);
 
 			sellout_yes.interactable = true;
 			sellout_no.interactable = true;
 		}
+		if (mode == 3) {
+			buyin_messenge_display.gameObject.SetActive (false);
+			sellout_messenge_display.gameObject.SetActive (false);
+			confirm_messenge_display.gameObject.SetActive (true);
+
+			Confirm.interactable = true;
+		}
+		if (mode == 4) {
+			buyin_messenge_display.gameObject.SetActive (false);
+			sellout_messenge_display.gameObject.SetActive (false);
+			confirm_messenge_display.gameObject.SetActive (true);
+
+			Confirm.interactable = true;
+		}
+	}
+
+	public void Instant_All_Update(){
+		Network_Update MyNetScript = MyNet.GetComponent<Network_Update>();
+		MyNetScript.UpdateConnection (Name.text, time_idx);
+
+		All_Update.image.color = All_Update.colors.normalColor;
+		}
+
+	public void Instant_Stock_Update(){
+		Network_Update MyNetScript = MyNet.GetComponent<Network_Update>();
+		MyNetScript.UpdateConnection_Stock_Only (Name.text, time_idx);
+
+		Stock_Update.image.color = Stock_Update.colors.normalColor;
 	}
 }
